351880aa8ef52a7429a90403f50921b47332264e
Ariana Blake <a_blake69703@fanshaweonline.ca>
2019-09-20 00:11:37 -0400

    added video player

--- js/app.js
+++ js/app.js
@@ -4,8 +4,17 @@ import Team from "./modules/DataObject.js";
 (() => {
     console.log('fired!');
 
+    //Variables
+    let vidPlayer = document.querySelector('video'),
+        pausePlayBtn = document.querySelector('.pause'),
+        rewindBtn = document.querySelector('.rewind'),
+        replayBtn = document.querySelector('.replay'),
+        muteBtn = document.querySelector('.mute');
+
+
     const bioInfo = document.querySelector('.teamSectionBio').children;
 
+    //Functions
     function showTeamData() {
         //change data on page here
 
@@ -17,6 +26,42 @@ import Team from "./modules/DataObject.js";
         bioInfo[2].textContent = `Work Motivation: ${Team[currentTeam].motivation}`;
         bioInfo[3].textContent = `Interests: ${Team[currentTeam].interests}`;
         bioInfo[4].textContent = `Favourite Match: ${Team[currentTeam].match}`;
+    }
+
+    function pausePlay() {
+        //if video is paused button is play button
+        if (vidPlayer.paused === true){
+            vidPlayer.play();
+            document.querySelector('.pause').src = "images/pause.png";
+        } else { //otherwise, button is pause button
+            vidPlayer.pause();
+            document.querySelector('.pause').src = "images/play.png";
+        }
+    }
+
+    function replayVid() {
+        if (vidPlayer.paused === true){
+            vidPlayer.currentTime = 0;
+            vidPlayer.play();
+            document.querySelector('.pause').src = "images/pause.png";
+        } else {
+            vidPlayer.currentTime = 0;
+            vidPlayer.play();
+        }
+    }
+
+    function rewindVid() {
+        vidPlayer.currentTime -= 5;
+    }
+
+    function muteVid() {
+        if (vidPlayer.muted === true) {
+            vidPlayer.muted = false;
+            document.querySelector('.mute').src = "images/mute.png";
+        } else {
+            vidPlayer.muted = true;
+            document.querySelector('.mute').src = "images/unmute.png";
+        }
     }
 
     //making temporary variable 
@@ -38,6 +83,12 @@ import Team from "./modules/DataObject.js";
 
     }
 
-    debugger;
+    //debugger;
+
+    //Event Handling
+    pausePlayBtn.addEventListener('click', pausePlay); //pauses/plays the video when the button is clicked
+    rewindBtn.addEventListener('click', rewindVid); //rewinds video to the start when button is clicked		
+    muteBtn.addEventListener('click', muteVid); //mute and unmute video when button is clicked
+    replayBtn.addEventListener('click', replayVid); //rewinds video to the start when button is clicked
 
 })();
