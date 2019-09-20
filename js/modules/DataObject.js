c2278b56d955146c9ecb09f9c89175c210fd74da
Hillary <h_strong69707@fanshaweonline.ca>
2019-09-19 23:53:53 -0400

    Add oxford commas

--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -3,7 +3,7 @@ const Team = {
         name: "Sarah Conway",
         role: "Motion Designer, Co-Founder",
         motivation: "Making cinematic content that blows everyone’s freaking minds",
-        interests: "Cooking new dishes, doodling and ghosts",
+        interests: "Cooking new dishes, doodling, and ghosts",
         match: "Slow-cooker & Me!"
     },
 
@@ -11,7 +11,7 @@ const Team = {
         name: "Hillary Strong",
         role: "Graphic Designer, Co-Founder",
         motivation: "Passion to create an appealing brand for both my eyes and the client",
-        interests: "Testing out new restaurants in the London area, rock climbing and spin class",
+        interests: "Testing out new restaurants in the London area, rock climbing, and spin class",
         match: "Bacon & Eggs!"
     },
 
@@ -19,7 +19,7 @@ const Team = {
         name: "Ariana Blake",
         role: "Front-End Developer, Co-Founder",
         motivation: "Creating cool stuff that is both functional and aesthetically pleasing",
-        interests: "Playing video games, petting dogs and board game nights with friends",
+        interests: "Playing video games, petting dogs, and board game nights with friends",
         match: "Mac & Cheese!"
     },
     
